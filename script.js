29311c79988a9a74d1346b1c72601ee976b5bb51
mudi-igbinoba <ruth.igbinoba12@gmail.com>
2021-10-13 13:41:27 +0100

    made corrections to my arrays

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
-let dogArray = [];
-
-function Dog(image, name, price) {
-    this.image =image,
-    this.name =name,
-    this.price =price
-};
-
-let dog1 = new Dog("tiny-white.jpg", "Scottie", "$500");
-dogArray.push(dog1);
-
-let dog2 = new Dog("spaniel-puppy-playing-studio-cute-doggy-pet-is-sitting-isolated-blue-background-cavalier-king-charles-negative-space-insert-your-text-image-concept-movement-animal-rights_155003-33840.jpg", "Lady", "$450");
-dogArray.push(dog2);
-
-let dog3 = new Dog("long-hair.jpeg", "Dawn", "$600");
-dogArray.push(dog3);
-
-let dog4 = new Dog("head-sweater.jpeg", "Bingo", "$1000");
-dogArray.push(dog4);
-
-let dog5 = new Dog("fluffy.jpeg", "Fluffy", "$2000");
-dogArray.push(dog5);
-
-let dog6 = new Dog("dalmatian-1.jpeg", "Wolf", "$700");
-dogArray.push(dog6);
-
-let dog7 = new Dog("brown-spots.jpeg", "Simba", "$1200");
-dogArray.push(dog7);
-
-let dog8 = new Dog("beagles-puppies-looking-something_1150-18193.jpg", "Zama", "$200");
-dogArray.push(dog8);
-
-let dog9 = new Dog("hugger.jpg", "Beamer", "$900");
-dogArray.push(dog9);
-
-let dog10 = new Dog("front-view-beautiful-dog-with-copy-space.jpg", "Einstein", "$1000");
-dogArray.push(dog10);
-
-let dog11 = new Dog("tiny-black.jpeg", "Brownie", "$400");
-dogArray.push(dog11);
-
-let dog12 = new Dog("big-eyes.jpeg", "Charlie", "$600");
-dogArray.push(dog12);
+let dogArray = [
+{ 
+    image:"tiny-white.jpg", 
+    name:"Scottie", 
+    price:"$500"
+},
+{
+    image:"spaniel-puppy-playing-studio-cute-doggy-pet-is-sitting-isolated-blue-background-cavalier-king-charles-negative-space-insert-your-text-image-concept-movement-animal-rights_155003-33840.jpg", 
+    name:"Lady", 
+    price:"$450"
+},
+{
+    image:"long-hair.jpeg", 
+    name:"Dawn", 
+    price:"$600"
+},
+{
+    image:"head-sweater.jpeg", 
+    name:"Bingo", 
+    price:"$1000"
+},
+{
+    image:"fluffy.jpeg", 
+    name:"Fluffy", 
+    price:"$2000"
+},
+{
+    image:"dalmatian-1.jpeg", 
+    name:"Wolf", 
+    price:"$700"
+},
+{
+    image:"brown-spots.jpeg", 
+    name:"Simba", 
+    price:"$1200"
+},
+{
+    image:"beagles-puppies-looking-something_1150-18193.jpg", 
+    name:"Zama", 
+    price:"$200"
+},
+{
+    image:"hugger.jpg", 
+    name:"Beamer", 
+    price:"$900"
+},
+{
+    image:"front-view-beautiful-dog-with-copy-space.jpg", 
+    name:"Einstein", 
+    price:"$1000"
+},
+{
+    image:"tiny-black.jpeg", 
+    name:"Brownie", 
+    price:"$400"
+},
+{
+    image:"big-eyes.jpeg",
+     name:"Charlie", 
+     price:"$600"
+},]
 console.log(dogArray);
 
 let gallery = document.querySelector('.row');
